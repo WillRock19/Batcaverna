68ae2e8998fd90e223e8be6dc4f25970da6f0aaf
William <william.pinto@lambda3.com.br>
2020-10-28 20:59:20 -0300

    Adicionados métodos ao repositório; criada modelo de BatTraje

---
+++ src/Repositorios/BatTrajeRepositorio.cs
@@ -0,0 +1,26 @@
+﻿using Batcaverna.Modelos;
+using System.Collections.Generic;
+
+namespace Batcaverna.Repositorios
+{
+    public class BatTrajeRepositorio
+    {
+        private List<BatTraje> _listaDeTrajes;
+
+        public BatTrajeRepositorio()
+        {
+            _listaDeTrajes = new List<BatTraje>();
+        }
+
+        public void AdicionarTraje(BatTraje traje) 
+        {
+            _listaDeTrajes.Add(traje);
+        }
+
+
+        public List<BatTraje> ListarTrajes() 
+        {
+            return _listaDeTrajes;
+        }
+    }
+}
