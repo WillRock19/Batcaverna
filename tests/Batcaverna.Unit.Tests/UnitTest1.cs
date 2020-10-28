0de2f124835d8d5c4382429a9a568f37e0b15c42
William <william.pinto@lambda3.com.br>
2020-10-28 20:37:12 -0300

    Criado projeto de testes de Unidade

---
+++ tests/Batcaverna.Unit.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Batcaverna.Unit.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
