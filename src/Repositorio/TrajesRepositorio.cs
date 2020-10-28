9b512c7804e8ce0181169622edd582173c00fbac
William <william.pinto@lambda3.com.br>
2020-10-28 20:49:56 -0300

    Criado repositório para armazenar trajes

---
+++ src/Repositorio/TrajesRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Batcaverna.Repositorio
+{
+    public class TrajesRepositorio
+    {
+        private List<string> _listaDeTrajes;
+
+        public TrajesRepositorio()
+        {
+            _listaDeTrajes = new List<string>();
+        }
+
+        public void AdicionarTraje(string traje) 
+        {
+            _listaDeTrajes.Add(traje);
+        }
+
+    }
+}
