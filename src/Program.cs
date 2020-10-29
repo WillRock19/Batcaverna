76d1863a4568f73574afe60629e5a7c74e44ad5c
William <william.pinto@lambda3.com.br>
2020-10-28 21:13:55 -0300

    Criado menu inicial

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Batcaverna.Repositorios;
+using System;
 
 namespace Batcaverna
 {
@@ -6,7 +7,24 @@ namespace Batcaverna
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var opcaoSelecionada = "";
+            Console.WriteLine("Bem-vindo à Batcaverna!");
+
+            while (opcaoSelecionada != "x") 
+            {
+                Console.WriteLine("O que deseja fazer?");
+                Console.WriteLine("1. Verificar quantidade de BatTrajes");
+                Console.WriteLine("X. Sair");
+
+                opcaoSelecionada = Console.ReadLine();
+
+                if(opcaoSelecionada == "1") 
+                {
+                    var repositorio = new BatTrajeRepositorio();
+                    var quantidadeDeTrajes = repositorio.ListarTrajes().Count;
+                    Console.WriteLine($"Quantidade de trajes: {quantidadeDeTrajes}\n");
+                }
+            }
         }
     }
 }
