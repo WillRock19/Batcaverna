b5c6b7c1a2469444d4e768715aa8e90ed655a95c
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2020-11-11 16:30:50 -0300

    Criado o modelo BatCarro e repositorio BatCarroRepositorio

---
+++ src/Modelos/BatCarro.cs
@@ -0,0 +1,13 @@
+using Batcaverna.Modelos.Acessorios;
+
+namespace Batcaverna.Modelos
+{
+    public class BatCarro
+    {
+        public long Id { get; set; }
+        public bool ResistenteÀCorte { get; set; }
+        public bool ResistenteÀBalas { get; set; }
+        public string CorPrincipal { get; set; }
+        
+    }
+}
