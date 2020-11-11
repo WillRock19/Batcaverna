d3a201fe5095199ee6cc0ca4a32e1cd8bf235e17
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2020-11-11 16:47:22 -0300

    Adicionada propriedades ao BatCarro

--- src/Modelos/BatCarro.cs
+++ src/Modelos/BatCarro.cs
@@ -8,6 +8,7 @@ namespace Batcaverna.Modelos
         public bool ResistenteÀCorte { get; set; }
         public bool ResistenteÀBalas { get; set; }
         public string CorPrincipal { get; set; }
-        
+        public bool RodasResistenteÀBalas { get; set; }
+        public bool RodasResistenteÀCorte { get; set; }        
     }
 }
