480a2155544871d1ba654f4b1048bac9af7fb865
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2020-11-11 17:01:26 -0300

    Adicionado as opções Cadastro,Quantidade e Verificação do carro ao menu

--- src/Batcave.cs
+++ src/Batcave.cs
@@ -9,7 +9,8 @@ namespace Batcaverna
         static void Main(string[] args)
         {
             var opcaoSelecionada = "";
-            var repositorio = new BatTrajeRepositorio();
+            var repositorioT = new BatTrajeRepositorio();
+            var repositorioC = new BatCarroRepositorio();
 
             Console.WriteLine("Bem-vindo à Batcaverna!");
 
@@ -19,13 +20,13 @@ namespace Batcaverna
 
                 if (opcaoSelecionada == "1") 
                 {
-                    repositorio.AdicionarTraje(new BatTraje());
+                    repositorioT.AdicionarTraje(new BatTraje());
                     Console.WriteLine($"Traje adicionado com sucesso :) \n");
                 }
 
                 if(opcaoSelecionada == "2") 
                 {
-                    var quantidadeDeTrajes = repositorio.ListarTrajes().Count;
+                    var quantidadeDeTrajes = repositorioT.ListarTrajes().Count;
                     Console.WriteLine($"Quantidade de trajes: {quantidadeDeTrajes}\n");
                 }
 
@@ -35,12 +36,38 @@ namespace Batcaverna
                     Console.WriteLine("Favor, informar o Id: ");
                     var idInformado = Console.ReadLine();
 
-                    var traje = repositorio.ObterPorId(Int64.Parse(idInformado));
+                    var traje = repositorioT.ObterPorId(Int64.Parse(idInformado));
 
                     if (traje != null)
                         Console.WriteLine($"O traje existe! \n");
                     else
                         Console.WriteLine("Traje não encontrado :/ \n");
+                }
+
+                if (opcaoSelecionada == "4") 
+                {
+                    repositorioC.AdicionarCarro(new BatCarro());
+                    Console.WriteLine($"Carro adicionado com sucesso :) \n");
+                }
+
+                if(opcaoSelecionada == "5") 
+                {
+                    var quantidadeDeCarros = repositorioC.ListarCarros().Count;
+                    Console.WriteLine($"Quantidade de carros: {quantidadeDeCarros}\n");
+                }
+
+
+                if (opcaoSelecionada == "6")
+                {
+                    Console.WriteLine("Favor, informar o Id: ");
+                    var idInformado = Console.ReadLine();
+
+                    var carro = repositorioC.ObterPorId(Int64.Parse(idInformado));
+
+                    if (carro != null)
+                        Console.WriteLine($"O carro existe! \n");
+                    else
+                        Console.WriteLine("Carro não encontrado :/ \n");
                 }
             }
         }
@@ -51,6 +78,9 @@ namespace Batcaverna
             Console.WriteLine("1. Cadastrar BatTraje");
             Console.WriteLine("2. Quantidade de BatTrajes");
             Console.WriteLine("3. Verificar se traje existe");
+            Console.WriteLine("4. Cadastrar BatCarro");
+            Console.WriteLine("5. Quantidade de BatCarros");
+            Console.WriteLine("6. Verificar se carro existe");
             Console.WriteLine("X. Sair");
 
             return Console.ReadLine();
