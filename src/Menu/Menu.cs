887a28365945a4c62685e7590a6f07fcf54209f9
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2020-11-18 19:44:33 -0300

    Configuradas classe e metodos abstratos BatTraje

--- src/Menu/Menu.cs
+++ src/Menu/Menu.cs
@@ -1,35 +1,11 @@
-namespace src
+namespace Batcaverna.Menu
 {
-    public class Menu
+    public abstract class Menu
     {
-        private string opcao;
-        public string Tipo
-        {
-            get { return opcao; }
-            set { opcao = value; }
-        }
-
-        public Menu(string opcaoMenu)
-        {
-            this.opcao = opcaoMenu ;
-        }
-
-        public virtual void Cadastrar()
-        {
-
-        }
-        public virtual void Quantidade()
-        {
-
-        }
-        public virtual void Verificar()
-        {
-
-        }
-        public virtual void Excluir()
-        {
-            
-        }
+        public abstract void Cadastrar();
+        public abstract void Quantidade();
+        public abstract void Verificar();
+        public abstract void Excluir();
     }
 
 }
