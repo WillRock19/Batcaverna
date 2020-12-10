8b6816e59c83592db3a5217598e0590bb6aee3ed
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2020-12-09 23:56:22 -0300

    Criada Interface, Repositorio base e alterado nome de menu para menuBase

---
+++ src/Menu/MenuBase.cs
@@ -0,0 +1,58 @@
+using System;
+using src.Interface;
+using src.Repositorios;
+
+namespace Batcaverna.Menu
+{
+    public abstract class MenuBase<T> where T : IEntidadeCadastral
+    {
+        private string _entidade;
+        protected BatBaseRepositorio<T> _baseRepositorio;
+
+        protected MenuBase(string entidade,BatBaseRepositorio<T> baseRepositorio)
+        {
+            _entidade=entidade;
+            _baseRepositorio=baseRepositorio;
+        }
+
+        public abstract void Cadastrar();
+        public void Excluir()
+        {
+            var idInformado = "";
+            Console.WriteLine("Favor, informar o Id: ");
+            idInformado = Console.ReadLine();
+
+            var valorInformado = _baseRepositorio.ObterPorId(Int64.Parse(idInformado));
+
+            if (valorInformado != null)
+            {
+                _baseRepositorio.Excluir(_baseRepositorio.ObterPorId(Int64.Parse(idInformado)));
+                Console.WriteLine($"{_entidade} excluido com sucesso :) \n");
+            }
+            else
+            Console.WriteLine($"{_entidade} não encontrado :/ \n");
+        }
+
+        public void Quantidade()
+        {
+            var quantidade = _baseRepositorio.Listar().Count;
+            Console.WriteLine($"Quantidade de {_entidade}: {quantidade}\n");
+        }
+
+        public void Verificar()
+        {
+            var idInformado = "";
+            Console.WriteLine("Favor, informar o Id: ");
+            idInformado = Console.ReadLine();
+
+            var valorInformado = _baseRepositorio.ObterPorId(Int64.Parse(idInformado));
+
+            if (valorInformado != null)
+                Console.WriteLine($"O {_entidade} existe! \n");
+            else
+                Console.WriteLine($"{_entidade} não encontrado :/ \n");
+        }
+ 
+    }
+
+}
