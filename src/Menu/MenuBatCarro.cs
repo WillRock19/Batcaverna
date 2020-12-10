871efe9a82e51eb26161448ff13c6d0fe75cb5af
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2020-12-09 23:54:15 -0300

    Refatorado MenuBase,BatBaseRepositorio e Interface

--- src/Menu/MenuBatCarro.cs
+++ src/Menu/MenuBatCarro.cs
@@ -1,53 +1,18 @@
 using Batcaverna.Menu;
 using Batcaverna.Modelos;
 using Batcaverna.Repositorios;
+using src.Repositorios;
 using System;
 namespace src.MenuBatCarro
 {
-    public class MenuBatCarro : Menu
+    public class MenuBatCarro : MenuBase<BatCarro>
     {
-        public BatCarroRepositorio batCarros = new BatCarroRepositorio();
+        public MenuBatCarro() : base(typeof(BatCarro).Name, new BatCarroRepositorio()){}
         public override void Cadastrar()
         {
-            batCarros.AdicionarCarro(new BatCarro());
-            Console.WriteLine($"BatCarro adicionado com sucesso :) \n");
+            _baseRepositorio.Adicionar(new BatCarro());
+            Console.WriteLine($"{typeof(BatCarro).Name} adicionado com sucesso :) \n");
         }
 
-        public override void Excluir()
-        {
-            var idInformado = "";
-            Console.WriteLine("Favor, informar o Id: ");
-            idInformado = Console.ReadLine();
-
-            var carro = batCarros.ObterPorId(Int64.Parse(idInformado));
-
-            if (carro != null)
-            {
-                batCarros.ExcluirCarro(batCarros.ObterPorId(Int64.Parse(idInformado)));
-                Console.WriteLine($"BatCarro excluido com sucesso :) \n");
-            }
-            else
-            Console.WriteLine("BatCarro não encontrado :/ \n");
-        }
-
-        public override void Quantidade()
-        {
-            var quantidadeDeCarros = batCarros.ListarCarros().Count;
-            Console.WriteLine($"Quantidade de BatCarros: {quantidadeDeCarros}\n");
-        }
-
-        public override void Verificar()
-        {
-            var idInformado = "";
-            Console.WriteLine("Favor, informar o Id: ");
-            idInformado = Console.ReadLine();
-
-            var carro = batCarros.ObterPorId(Int64.Parse(idInformado));
-
-            if (carro != null)
-                Console.WriteLine($"O BatCarro existe! \n");
-            else
-                Console.WriteLine("BatCarro não encontrado :/ \n");
-        }
-    }
+   }
 }
--- src/Repositorios/BatCarroRepositorio.cs
+++ src/Repositorios/BatCarroRepositorio.cs
@@ -1,41 +1,6 @@
 using Batcaverna.Modelos;
-using System.Collections.Generic;
-using System.Linq;
-
+using src.Repositorios;
 namespace Batcaverna.Repositorios
 {
-    public class BatCarroRepositorio
-    {
-        private long _contadorRegistros;
-        private List<BatCarro> _listaDeCarros;
-
-        public BatCarroRepositorio()
-        {
-            _listaDeCarros = new List<BatCarro>();
-            _contadorRegistros = _listaDeCarros.Count;
-        }
-
-        public void AdicionarCarro(BatCarro carro) 
-        {
-            carro.Id = _contadorRegistros + 1;
-
-            _listaDeCarros.Add(carro);
-            _contadorRegistros++;
-        }
-
-        public List<BatCarro> ListarCarros() 
-        {
-            return _listaDeCarros;
-        }
-
-        public BatCarro ObterPorId(long id) 
-        {
-            var carro = _listaDeCarros.FirstOrDefault(batCarro => batCarro.Id == id);
-            return carro;
-        }
-        public void ExcluirCarro(BatCarro carro) 
-        {
-            _listaDeCarros.Remove(carro);
-        }
-    }
+    public class BatCarroRepositorio : BatBaseRepositorio<BatCarro>{}
 }
